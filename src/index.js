2ed23bddf5b8169242ba545a6fd0d5cb06a13440
Piyush Satyam <piyushsatya81998@gmail.com>
2026-06-13 22:28:46 +0530

    Multiple Route handlers, next and call stack prefernce

--- src/index.js
+++ src/index.js
@@ -6,11 +6,22 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get('/user/:userId/:username', (req, res) => {
-  const { userId, username } = req.params;
-  res.send(`Hello, ${username}! Your ID is ${userId}.`);
-});
+app.get('/users', [(req, res, next) => {
+  console.log("FIRST RESPONSE FROM SERVER");
+  res.send('Hello World!');
+  next();
 
-app.get(/\/search\/ab?c\/ab.*if\/.*fly$/, (req, res) => {
-  res.send('You have reached the search endpoint with a complex pattern!');
-}); 
+}, (req, res, next) => {
+  console.log("SECOND RESPONSE FROM SERVER");
+  res.send('2ND Hello World!');
+  next();
+}
+, (req, res, next ) => {
+  console.log("THIRD RESPONSE FROM SERVER");
+  res.send('3RD Hello World!');
+  next();
+}], (req, res, next) => {
+  console.log("FOURTH RESPONSE FROM SERVER");
+  res.send('4TH Hello World!');
+}
+);
