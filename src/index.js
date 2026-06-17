5a07e2cbf512d060450526ff594aa4eee6d7d667
Piyush Satyam <piyushsatya81998@gmail.com>
2026-06-18 02:18:50 +0530

    CRUD basics without data sanitisation

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ const User = require('./models/user');
 
 app.use(express.json());
 
-
 app.post('/signup', async (req, res) => {
   console.log(req.body);
 
@@ -20,8 +19,74 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
 
-// app.post('/signup', async (req, res) => {
+app.delete('/users', async (req, res) => {
+  try {
+    const usersId = req.body._id;
+
+    const deletedUser = await User.findByIdAndDelete({ _id: usersId });
+
+    if (!deletedUser) {
+      return res.status(404).send('User not found');
+    }
+
+    res.status(200).send('User deleted successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+
+app.patch('/users', async (req, res) => {
+  try {
+    const usersId = req.body._id;
+    const Data = req.body;
+
+    const updatedUser = await User.findByIdAndUpdate({ _id: usersId }, Data,);
+
+    if (!updatedUser) {
+      return res.status(404).send('User not found');
+    }
+
+    res.status(200).send('User updated successfully');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+});
+
+
+// This route is for testing purposes to find users by email. In a real application, you would typically have a more secure and comprehensive user management system.
+// app.get('/users', async (req, res) => {
+//   try {
+//     const usersEmail = req.body.email;
+//     const users = await User.find({ email: usersEmail });
+
+//     if (users.length === 0) {
+//       return res.status(404).send('No users found with the provided email');
+//     } else {
+//       res.status(200).json(users);
+//       console.log(users);
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send(error.message);
+//   } 
+// });
+
+
+
+//app.post('/signup', async (req, res) => { 
 //   const userObject = {
 //     firstName: "Bhuttu",
 //     lastName: "Chawanni",
