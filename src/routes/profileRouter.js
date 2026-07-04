de1f74f10f542bcdff1ebd3902ec99b89cfc3429
Piyush Satyam <piyushsatya81998@gmail.com>
2026-07-04 22:41:18 +0530

    Small edit

--- src/routes/profileRouter.js
+++ src/routes/profileRouter.js
@@ -8,10 +8,10 @@ profileRouter.get('/profile/view', userAuth, async (req, res) => {
   try {
     const user = req.user;
 
-    res.status(200).json("User name: " + user.firstName + " " + user.lastName);
+    res.status(200).json(user);
 
   } catch (error) {
-    console.error(error);
+    console.error("Profile View Error:", error);
     res.status(500).send(error.message);
   }
 });
