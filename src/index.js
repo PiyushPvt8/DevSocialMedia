b912a74407939de2f8c98b63e328ad1cb1ddfb8e
Piyush Satyam <piyushsatya81998@gmail.com>
2026-07-10 22:30:53 +0530

    .env edit 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
-const port = process.env.PORT_ID;
+const port = process.env.PORT || 1998;
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
